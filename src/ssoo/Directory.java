b81a410ce3c77a84b825104edd586a48b300fae2
marqui <157283925+maarqui@users.noreply.github.com>
2025-05-12 09:46:06 +0200

    Update Directory.java
    herencia de GenericFile

--- src/ssoo/Directory.java
+++ src/ssoo/Directory.java
@@ -3,7 +3,7 @@ package ssoo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Directory {
+public class Directory extends GenericFile{
     String nombre;
     int clusterNumber;
     List<GenericFile> content = new ArrayList<GenericFile>();
